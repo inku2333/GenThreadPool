c327072a0c4bf805a3e27ca013f6fd98807be456
云间 <inku2333@users.noreply.github.com>
2025-07-18 14:28:06 +0800

    Update GenThreadPool.h
    删除注释，可以查看历史版本；
    优化代码，添加右值push（std::move）；

--- GenThreadPool.h
+++ GenThreadPool.h
@@ -27,6 +27,15 @@ public:
         m_condi.notify_one();
     }
 
+//    void push(T&& value)
+//    {  // 添加右值引用版本
+//        {
+//            std::lock_guard<std::mutex> lock(m_lock);
+//            m_queue.push(value);
+//        }
+//        m_condi.notify_one();
+//    }
+
     bool pop(T& value)
     {
         std::unique_lock<std::mutex> lock(m_lock);
@@ -74,25 +83,6 @@ public:
 
     bool pop(T& value)
     {
-//swapQueue这样写有死锁风险(配合原cancel一起)：
-//若生产者线程持有m_produceLock时，cancel尝试获取m_consumeLock（阻塞）
-//此时‌消费者线程持有若m_consumeLock，swapQueue尝试获取m_produceLock（阻塞），会形成循环等待（死锁）
-//        std::unique_lock<std::mutex> lock(m_consumeLock);
-//        if(m_consumeQueue.empty() && swapQueue() == 0){
-//            return false;
-//        }
-
-
-//        std::unique_lock<std::mutex> lock1(m_produceLock); // 先获取生产锁
-//        std::unique_lock<std::mutex> lock2(m_consumeLock); // 再获取消费锁
-//        if(m_consumeQueue.empty()){
-//            m_condi.wait_for(lock1, m_timeout, [this](){
-//                return !m_produceQueue.empty() || m_cancel;
-//            });
-//            if (m_produceQueue.empty()) return false;
-//            std::swap(m_produceQueue, m_consumeQueue);
-//        }
-
         std::unique_lock<std::mutex> lock(m_consumeLock);
         if(m_consumeQueue.empty() && swapQueue() == 0){
             return false;
@@ -106,8 +96,6 @@ public:
     // std::atomic<bool>取消cancel的加锁,避免死锁
     void cancel()
     {
-//        std::lock_guard<std::mutex> lock1(m_produceLock);
-//        std::lock_guard<std::mutex> lock2(m_consumeLock);
         m_cancel.store(true);
         m_condi.notify_all();
     }
@@ -119,6 +107,7 @@ private:
         m_condi.wait_for(lock, m_timeout, [this](){
             return !m_produceQueue.empty() || m_cancel;
         });
+        if (m_produceQueue.empty()) return 0;  // 超时或取消
         std::swap(m_produceQueue, m_consumeQueue);
         return m_consumeQueue.size();
     }
@@ -155,8 +144,8 @@ public:
 private:
     void work();
 
-    GenBlockQueue<std::function<void ()>> m_taskQueue;
-    //    GenBlockDoubleQueue<std::function<void ()>> m_taskQueue;
+//    GenBlockQueue<std::function<void ()>> m_taskQueue;
+    GenBlockDoubleQueue<std::function<void ()>> m_taskQueue;
     std::vector<std::thread> m_workers;
 
 };
